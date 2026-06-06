85a2a385ac3a50e7f05b2ccb403b7112ed28f1ea
Roman Matej <matej.roman@webcom.cz>
2026-06-06 15:36:59 +0200

    feat: implement article, header, and welcome components with styles

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,18 @@
 // import a css file containig article styles
+import "./Article.css" 
+
 
 // Create an Article function returning the HTML of article block
+const Article = () => {
+    return(
+        <article className="article">
+            <h2>Headline</h2>
+            <p className="article__paragraph">
+                This is a sample article paragraph text.
+            </p>
+        </article>
+    );
+};
 
 // Add a default export statement for Article component to use it in the other files
+export default Article;
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,14 @@
 // import a css file containig welcome styles
-
+import './Welcome.css'
 // Create a Welcome function returning the HTML of welcome block
 
+const Welcome = () => {
+    return(
+        <welcome className='welcome'>
+            <h1 className='welcome__text'>Sticky Header!</h1>
+        </welcome>
+    );
+}
+
 // Add a default export statement for Welcome component to use it in the other files
+export default Welcome;
